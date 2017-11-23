a6630c1335b1eba8304a78fca8f20bd1a631de97
hebin123456 <654190921@qq.com>
2017-11-23 09:23:10 +0800

    upload
    upload

---
+++ 计时器/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace 计时器
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 15F);
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "时间：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 15F);
+            this.label2.Location = new System.Drawing.Point(79, 10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "00:00:00";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 50);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(40, 40);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(72, 50);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(40, 40);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(131, 50);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(40, 40);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(184, 100);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "计时器——By Hebin";
+            this.TopMost = true;
+            this.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDoubleClick);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ 计时器/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 计时器
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            // 标签也绑定双击事件
+            label1.MouseDoubleClick += Form1_MouseDoubleClick;
+        }
+
+        private int time = 0;
+        private bool isplayed = false;
+        // 开始
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            if (!isplayed)
+            {
+                timer1.Start();
+                isplayed = true;
+            }
+        }
+
+        // 暂停
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            if (isplayed)
+            {
+                timer1.Stop();
+                isplayed = false;
+            }
+        }
+
+        // 停止
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            label2.Text = "00:00:00";
+            time = 0;
+            isplayed = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            time += timer1.Interval / 1000;
+            int h = time / 3600;
+            int m = time / 60 - h * 60;
+            int s = time % 60;
+            label2.Text = string.Format("{0:D2}", h) + ":" + string.Format("{0:D2}", m) + ":" + string.Format("{0:D2}", s);
+        }
+
+        private bool hide = false;
+        private void Form1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (!hide)
+            {
+                this.Size = new Size(this.Size.Width, this.Size.Height - 55);
+                this.FormBorderStyle = FormBorderStyle.None;
+                Rectangle rec = Screen.GetWorkingArea(this);
+                this.Location = new Point((rec.Width - this.Size.Width) / 2, 0);
+                this.label2.MouseClick += label_Start_Pause;
+                this.label2.MouseDoubleClick += label_Stop;
+            }
+            else
+            {
+                this.Size = new Size(this.Size.Width, this.Size.Height + 55);
+                this.FormBorderStyle = FormBorderStyle.FixedSingle;
+                Rectangle rec = Screen.GetWorkingArea(this);
+                this.Location = new Point((rec.Width - this.Size.Width) / 2, (rec.Height - this.Size.Height) / 2);
+                this.label2.MouseClick -= label_Start_Pause;
+                this.label2.MouseDoubleClick -= label_Stop;
+            }
+            hide = !hide;
+        }
+
+        // 标签开始暂停手势
+        private void label_Start_Pause(object sender, MouseEventArgs e)
+        {
+            if (!isplayed)
+            {
+                timer1.Start();
+            }
+            else
+            {
+                timer1.Stop();
+            }
+            isplayed = !isplayed;
+        }
+
+        // 标签停止手势
+        private void label_Stop(object sender, MouseEventArgs e)
+        {
+            timer1.Stop();
+            label2.Text = "00:00:00";
+            time = 0;
+            isplayed = false;
+        }
+    }
+}
